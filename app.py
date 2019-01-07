9cbc3a375e635667bf5734ef4aaa9c29a585101a
Hugh Zhang <hughzhang@hughs-mbp.attlocal.net>
2019-01-06 17:16:25 -0800

    Server code
    Included the CURL text file as well as the Translation JSON key (PLEASE
    DO NOT ABUSE :O)
    
    Basic backtranslation between french and English

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+import json
+import subprocess
 import os
+from IPython import embed
+
 app = Flask(__name__)
- 
+
+with open("CURL.txt") as f:
+    curl = f.readline()
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Translation-5254a3fddf5e.json"
+
+def extract(httpcurl):
+    raw_response = subprocess.check_output(httpcurl, shell=True)
+    translation = json.loads(raw_response)['data']['translations'][0]['translatedText']
+    return translation
+
+def back_translate(text, mid='fr'):
+    forward = curl.replace("SOURCE", "en").replace("TARGET", mid).replace("QUERY", text)
+    translation = extract(forward)
+    backward = curl.replace("SOURCE", mid).replace("TARGET", "en").replace("QUERY", translation)
+    paraphrased = extract(backward)
+    return paraphrased
+
+def test():
+    base = "Fujisaki Station is a station on the Gonō line, 144.7 km from the terminus of the Higashi-Noshiro Line."
+    para = back_translate(base)
+    embed()
+    
 @app.route("/")
 def index():
 	return render_template('index.html')
  
-@app.route("/translate")
-def translate():
-	text = request.values.get('text')
-	return render_template('index.html',text = text, modified = text )
-
-
-
+@app.route("/paraphrase")
+def paraphrase():
+    text = request.values.get('text')
+    para = back_translate(text)
+    return render_template('index.html',text = text, modified = para)
 
 if __name__ == "__main__":
-	app.run(host = 'localhost', port = 80)
+    app.run(host = 'localhost', port = 4000)
