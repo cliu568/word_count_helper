54da5518d166397110da5c859de7b4deae0175a7
Allen <cliu568@mit.edu>
2019-01-06 23:10:13 -0500

    windows lol

--- app.py
+++ app.py
@@ -4,30 +4,51 @@ import subprocess
 import os
 from IPython import embed
 
+import platform
+
+
 app = Flask(__name__)
 
-with open("CURL.txt") as f:
-    curl = f.readline()
+if(platform.system() =='Windows'):
+    with open("CURL_windows.txt") as f:
+        curl = f.readline()
+
+else:
+    with open("CURL.txt") as f:
+        curl = f.readline()
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "Translation-a3cfdf2016a1.json"
 
+
+def test():
+    print(curl)
+    base = "Fujisaki Station is a station on the Gonō line, 144.7 km from the terminus of the Higashi-Noshiro Line."
+    para = back_translate(base)
+
+    embed()
+    
+
+
+
 def extract(httpcurl):
     raw_response = subprocess.check_output(httpcurl, shell=True)
-    translation = json.loads(raw_response)['data']['translations'][0]['translatedText']
+    
+    translation = json.loads(raw_response.decode('utf-8'))['data']['translations'][0]['translatedText']
     return translation
 
 def back_translate(text, mid='fr'):
     forward = curl.replace("SOURCE", "en").replace("TARGET", mid).replace("QUERY", text)
     translation = extract(forward)
+    
+
     backward = curl.replace("SOURCE", mid).replace("TARGET", "en").replace("QUERY", translation)
+    
     paraphrased = extract(backward)
     return paraphrased
 
-def test():
-    base = "Fujisaki Station is a station on the Gonō line, 144.7 km from the terminus of the Higashi-Noshiro Line."
-    para = back_translate(base)
-    embed()
-    
+
+
+
 @app.route("/")
 def index():
 	return render_template('index.html')
